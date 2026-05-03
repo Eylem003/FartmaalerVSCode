d16889583b8efea1dc3acc52e14fc6641a957bea
Eylem003 <eylem310305@gmail.com>
2026-05-03 21:11:34 +0200

    Elev - ny måling

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-function goToStudent() {
-
-    let codeInput = document.querySelector(".student-access input").value;
-
-    if (codeInput.trim() === "") {
-        alert("Indtast en kode først");
-        return;
-    }
-
+function goToMeasurement () {
     window.location.href = "elev.html";
 }
